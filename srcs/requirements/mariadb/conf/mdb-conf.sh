aa9a1c81ed0693eb2ea9f638ec5235d8ad570041
Pietro Falaschi <falaschip@gmail.com>
2024-08-07 11:10:02 +0200

    push

--- srcs/requirements/mariadb/conf/mdb-conf.sh
+++ srcs/requirements/mariadb/conf/mdb-conf.sh
@@ -7,5 +7,10 @@ mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \`${DB_USER}\`@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 
 
+echo "[mysqld]" >> /etc/mysql/my.cnf
+echo "port = 3306" >> /etc/mysql/my.cnf
+echo "bind-address = 0.0.0.0" >> /etc/mysql/my.cnf
+echo "datadir = /var/lib/mysql" >> /etc/mysql/my.cnf
+
 mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
